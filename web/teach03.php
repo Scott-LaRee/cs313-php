da012c6173003aff98db6a36175fe7db3b4b659e
LaRee <sco17020@byui.edu>
2019-09-30 09:59:30 -0700

    add files for week 3 team activity

---
+++ web/teach03.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+<head>
+<meta charset="utf-8">
+<title>Week 3 Team Activity</title>
+<!--<link id="styleOfPage" type="text/css" rel="StyleSheet"
+    href="teach02.css" />
+-->
+</head>
+<body>
+<form method="POST" action="w3handle.php">
+<input type="text" name="name">
+<input type="text" name="email">
+<input type="radio" name="major" value="Computer Science">Computer Science<br>
+<input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
+<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
+<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+<textarea row="4" cols="50" id="comments">
+</textarea>
+<input type="checkbox" name="continents[]" value="North America">North America<br>
+<input type="checkbox" name="continents[]" value="South America">South America<br>
+<input type="checkbox" name="continents[]" value="Europe">Europe<br>
+<input type="checkbox" name="continents[]" value="Asia">Asia<br>
+<input type="checkbox" name="continents[]" value="Australia">Australia<br>
+<input type="checkbox" name="continents[]" value="Africa">Africa<br>
+<input type="checkbox" name="continents[]" value="Antarctica">Antarctica<br>
+<input type="submit">
+</form>
+</body>
+</html>
---
+++ web/w3handle.php
@@ -0,0 +1,39 @@
+<?php
+$name = $email= $major = $comments  = "";
+
+if($_SERVER["REQUEST_METHOD"] == POST) {
+	$name = clean_input($_POST["name"]);
+	$email = clean_input($_POST["email"]);
+	$major = clean_input($_POST["major"]);
+	$comments = clean_input($_POST["comments"]);
+}
+
+function clean_input($data) {
+	$data = trim$($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+
+
+<!DOCTYPE HTML>
+<html lang="en-us">
+<head>
+<meta charset="utf-8">
+<title>Week 3 Team Activity</title>
+</head>
+<body>
+ echo "Name: " . $name . " <br>";
+ echo "Email: " . $email . "<br>";
+ echo "Major: " . $major . "<br>";
+ echo "Comments: " . $comments . "<br>";
+ 
+ if(!empty($_POST['continents'])) {
+	foreach($_POST['continents'] as selected)
+	echo $selected."</br>";
+ }
+</body>
+</html>
+
+?>
