9efa9507cecf982351bed1b0916cce91612f9cb7
LaRee <sco17020@byui.edu>
2019-09-30 10:04:09 -0700

    correct form layout add labels

--- web/teach03.php
+++ web/teach03.php
@@ -9,14 +9,16 @@
 </head>
 <body>
 <form method="POST" action="w3handle.php">
-<input type="text" name="name">
-<input type="text" name="email">
+<input type="text" name="name"><label for="name">Name</label><br>
+<input type="text" name="email"><label for="email">Email</label><br>
+<label for="major">Major</label><br>
 <input type="radio" name="major" value="Computer Science">Computer Science<br>
 <input type="radio" name="major" value="Web Design and Development">Web Design and Development<br>
 <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
 <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
 <textarea row="4" cols="50" id="comments">
-</textarea>
+</textarea><br>
+<label for="continents[]">Continents Visited</label><br>
 <input type="checkbox" name="continents[]" value="North America">North America<br>
 <input type="checkbox" name="continents[]" value="South America">South America<br>
 <input type="checkbox" name="continents[]" value="Europe">Europe<br>
