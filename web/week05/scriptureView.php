73938240630881f8c831a54f5dd4553cfcd79978
LaRee <sco17020@byui.edu>
2019-10-16 22:28:09 -0700

    add php for team activity to web/week05

---
+++ web/week05/scriptureView.php
@@ -0,0 +1,40 @@
+<?php 
+/***
+* scriptureView.php
+* LaRee Scott
+*
+* cennect to scriptures database on heroku
+*/
+
+try 
+{
+	$dbUrl = getenv('DATABASE_URL');
+	
+	$dbOpts = parse_url($dbURL);
+	
+	$dbHost = $dbOpts["host"];
+	$dbPort = $dbOpts["port"];
+	$dbUser = $dbOpts["user"];
+	$dbPassword = $dbOpts["pass"];
+	$dbName = ltrim($dbOpts["path"], '/');
+	
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOEXCEPTION $ex)
+{
+	echo 'Error! ' . $ex->getMessage();
+	die();
+}
+
+echo '<h1>Scripture Resources</h1>'
+
+foreach($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+{
+	echo "<p>";
+	echo $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . ' - \"' . $row['content'] . '\"';
+	echo "</p>";
+}
+?>
+
+
+
