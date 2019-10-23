bb0e400a787e9593fb7dd7b7517f360a2c05e613
LaRee <sco17020@byui.edu>
2019-10-22 21:28:04 -0700

    add page framework

--- web/project1/viewStudent.php
+++ web/project1/viewStudent.php
@@ -5,8 +5,45 @@
 */
 
 require "dbConnect.php";
+?>
+<!DOCTYPE HTML>
+<html lang="en-us">
+<head>
+<meta charset="utf-8">
+<title>Student info</title>
+<!--<link id="styleOfPage" type="text/css" rel="StyleSheet" 
+	href="project1.css" />-->
 
-  //$first = 'John';
+</head>
+<body>
+   <div id="page">
+  <div id="head">
+   <header>
+    
+   <div id="pageHead">
+    <h1>STUDENT INFO</h1>
+   </div>
+   <div id="menuBar">
+    <ul id="menuBarList">
+     <li class="menuBarItem"><a href="fbla.html">HOME</a></li>
+     <li class="menuBarItem"><a href="student.html">STUDENT INFO</a></li>
+     <li class="menuBarItem"><a href="meetings.html">MEETINGS</a></li>
+	 <li class="menuBarItem"><a href="events.html">EVENTS</a></li>
+    </ul>
+   </div>
+   </header>
+ </div>
+ <div id="sideBar">
+<div id="sideBarList">
+    <div class="sideBarItem"><h3><a href="student.html">STUDENT</a></h3></div>
+    <div class="sideBarItem"><h3><a href="meetings.html">MEETINGS</a></h3></div>
+    <div class="sideBarItem"><h3><a href ="events.html">EVENTS</a></h3></div>
+  </div>
+</div>
+ <div id="content">
+  <div>
+    <?php
+	  //$first = 'John';
   $first = "'" . validate($_POST['first_view']) . "'";
   //$last = 'Doe';
   $last = "'" . validate($_POST['last_view']) . "'";
@@ -60,54 +97,15 @@ require "dbConnect.php";
   {
 	  echo $sql . "<br>" . $ex->getMessage();
   }
-  
-  $db = null;*/
-  /*
-  try {
-    $sql = "SELECT * FROM student WHERE student.student_first_name = 'John'
-	AND student.student_last_name = 'Doe'";
-
-    $db->exec($sql);
-	
-	echo $db;
-	} 
-  catch (PDOException $ex)
-  {
-	  echo $sql . "<br>" . $ex->getMessage();
-  }
-  
-  /*echo $first . " " . $last;*/
-  /* query excutes but gets non existent column error.
-  try {
-    $sql = "SELECT * FROM student WHERE student.student_first_name = $first
-		 AND student.student_last_name = $last;";
-
-    $db->exec($sql);
-	
-	echo $db;
-	} 
-  catch (PDOException $ex)
-  {
-	  echo $sql . "<br>" . $ex->getMessage();
-  }
-  *//*
-  try {
-	  $sql = $pdo->prepare('SELECT * FROM student WHERE student.student_first_name = ?
-		AND student.student_last_name = ?');
-	  
-	  $sql->execute([$first, $last]);
-	  
-	  $user = $sql->fetch();
-	  
-	  
-  }
-  catch (PDOException $ex)
-  {
-	  echo $sql . "<br>". $ex->getMessage();
-  }*/
-  
-  $db = null;
-  echo "page executed";
-?>
-
+  */
+  ?>
+  </div>
  
+ </div>
+<!--<script type="text/javascript" src="project1.js" charset="UTF-8"></script>-->
+</div>
+</body>
+</html>
+
+
+  
