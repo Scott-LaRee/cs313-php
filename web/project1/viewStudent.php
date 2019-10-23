4d4a2699dc575fa7107b8b2fd37c1d27d19e611e
LaRee <sco17020@byui.edu>
2019-10-22 21:13:49 -0700

    try to pattern after viewAllStudents

--- web/project1/viewStudent.php
+++ web/project1/viewStudent.php
@@ -16,8 +16,44 @@ require "dbConnect.php";
 	  return $data;
   }
   
-  /*echo $first . " " . $last;*/
+  try {
+    $sql = "SELECT * FROM student WHERE student.student_first_name = 'John'
+		AND student.student_last_name = 'Doe'";
+	
+	foreach($db->query($sql) as $row) 
+	{
+		print "<br/>";
+		print $row['student_first_name'] . '-' . $row['student_last_name'];
+		print "-" . $row['grad_year'] . '-' . $row['membership'];
+		print "-" . $row['office'] . '<br/>';
+	}
+
+    $db->exec($sql);
+	
+	echo $db;
+	} 
+  catch (PDOException $ex)
+  {
+	  echo $sql . "<br>" . $ex->getMessage();
+  }
   
+  $db = null;
+  /*
+  try {
+    $sql = "SELECT * FROM student WHERE student.student_first_name = 'John'
+	AND student.student_last_name = 'Doe'";
+
+    $db->exec($sql);
+	
+	echo $db;
+	} 
+  catch (PDOException $ex)
+  {
+	  echo $sql . "<br>" . $ex->getMessage();
+  }
+  
+  /*echo $first . " " . $last;*/
+  /* query excutes but gets non existent column error.
   try {
     $sql = "SELECT * FROM student WHERE student.student_first_name = $first
 		 AND student.student_last_name = $last;";
@@ -30,6 +66,21 @@ require "dbConnect.php";
   {
 	  echo $sql . "<br>" . $ex->getMessage();
   }
+  *//*
+  try {
+	  $sql = $pdo->prepare('SELECT * FROM student WHERE student.student_first_name = ?
+		AND student.student_last_name = ?');
+	  
+	  $sql->execute([$first, $last]);
+	  
+	  $user = $sql->fetch();
+	  
+	  
+  }
+  catch (PDOException $ex)
+  {
+	  echo $sql . "<br>". $ex->getMessage();
+  }*/
   
   $db = null;
   echo "page executed";
