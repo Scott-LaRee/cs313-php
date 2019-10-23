6aa2424b52f292894792b787019ae5dabcdac354
LaRee <sco17020@byui.edu>
2019-10-23 16:10:34 -0700

    dbConnect

---
+++ web/week06/dbConnect.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* dbConnect.php
+*
+* connects to database
+*/
+
+try {
+$url = getenv('DATABASE_URL');
+
+$dbOpts =  parse_url($url);
+
+$host = $dbOpts["host"];
+$port = $dbOpts["port"];
+$user = $dbOpts["user"];
+$pass = $dbOpts["pass"];
+$name = ltrim($dbOpts["path"], '/');
+
+$db = new PDO ("pgsql:host=$host; port=$port; dbname=$name", $user, $pass);
+
+$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex) {
+	echo "ERROR connecting to DB. Detains: $ex";
+	die();
+}
+
+return $db;
+
+?>
