55744c2a3eb8648b715cb15e6ccdd3ee943fc50b
LaRee <sco17020@byui.edu>
2019-11-02 17:54:17 -0700

    fix typo

--- web/project1/viewAllStudents.php
+++ web/project1/viewAllStudents.php
@@ -45,7 +45,7 @@ ini_set('display_errors', 1);
 		echo "<table>";
 		echo "<tr><th>Student</th><th>Graduation Year</th><th>Membership</th>";
 		echo "<th>Office Held</th></tr>";
-		echo "<tr><td>$row['student_first_name'] . ' '";
+		echo "<tr><td>" . $row['student_first_name'] . " ";
         echo $row['student_last_name'] . "</td>";
 		echo "<td>" . $row['grad_year'] . '</td><td>';
 		echo $row['membership'] . "</td><td>" . $row['office'] . '</td>';
