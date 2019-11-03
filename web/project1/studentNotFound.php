30b2a820b310466efdd26b1cffc20fbd959af1d3
LaRee <sco17020@byui.edu>
2019-11-02 22:33:14 -0700

    add file studentNotFound.php

---
+++ web/project1/studentNotFound.php
@@ -0,0 +1,13 @@
+<?php
+  $query = 'INSERT INTO student (student_first_name,
+           student_last_name) VALUES (:first, :last)';
+	  				
+  $sql = $db->prepare($query);
+  $sql->bindValue(':first', $first);
+  $sql->bindValue(':last', $last);
+  $sql->execute();
+	 
+  echo "Student $first $last added";
+				  
+  $studentId = $db->lastInsertId("student_id_seq");	 
+?>
